58e5bf1700ef564c2487b0d20c2bf5242b166a5a
daniel-black <drblack651@gmail.com>
2023-12-07 18:09:45 -0700

    functions for parsing load data from string

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { getFilePath } from "./io.js";
+import { getFilePath, getLoadsFromFileContent, readProblemFile } from "./io.js";
 
 // read a file path from the command line args
 const path = getFilePath();
+
+// read the file data
+const fileContent = readProblemFile(path);
+
+// get loads from the data
+const loads = getLoadsFromFileContent(fileContent);
+
 console.log("path: ", path);
+console.log("\nloads:\n", loads);
